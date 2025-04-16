89be5f25e549d4ec47d1c7d309b34ad51542d204
gpupo <g1mar@gpupo.com>
2025-04-16 06:43:24 -0300

    parse event

--- orchestration/scanear_choreography/scanear_flow.py
+++ orchestration/scanear_choreography/scanear_flow.py
@@ -10,34 +10,50 @@ Execute esse arquivo e dispare o evento em outro terminal
 
 """
 
+# ic.configureOutput(prefix="-> ", outputFunction=print)
+import ast
+
 from icecream import ic
 from prefect import flow
 from prefect.events import DeploymentEventTrigger
-from utils.prefect import PrefectEvent
-
-ic.configureOutput(prefix="-> ", outputFunction=print)
+from utils.prefect import parse_event_data
 
 
 @flow(name="scanear-flow")
-def scanear_flow(message, event_data):
-    ic(message)
-    ic(event_data)
-    event = PrefectEvent(**event_data)
-    ic(event.payload)
+def scanear_flow(
+    deployment_parameters,
+    id,
+    occurred,
+    event,
+    payload,
+):
+    event = parse_event_data(
+        deployment=deployment_parameters,
+        occurred=occurred,
+        event=event,
+        payload=payload,
+        id=id,
+    )
+    ic(event)
+    ic(event.message)
 
 
+EVENT_PARSE_PARAMETERS_TEMPLATE = {
+    "id": "{{ event.id }}",
+    "occurred": "{{ event.occurred }}",
+    "event": "{{ event.event }}",
+    "payload": "{{ event.payload }}",
+}
 if __name__ == "__main__":
     scanear_flow.serve(
         name="scanear-deployment",
-        parameters={"message": {"automation": "trigger"}},
+        parameters={"deployment_parameters": {"automation": "trigger"}},
         triggers=[
             DeploymentEventTrigger(
                 enabled=True,
                 match={"prefect.resource.id": "my.external.resource"},
                 expect=["scaneou"],
-                parameters={
-                    "event_data": "{{ event }}",
-                },
+                parameters=EVENT_PARSE_PARAMETERS_TEMPLATE,
             )
         ],
     )
--- orchestration/utils/prefect.py
+++ orchestration/utils/prefect.py
@@ -1,32 +1,40 @@
-from datetime import datetime
+import ast
+from datetime import datetime, timezone
 from typing import Any, Dict, List, Optional
 from uuid import UUID
-from zoneinfo import ZoneInfo
 
-from pydantic import BaseModel, Field
-from pydantic.functional_validators import field_validator
+from pydantic import BaseModel, Field, computed_field
 
 
-class Resource(BaseModel):
-    root: Dict[str, Any]
+def parse_event_data(**kwargs):
+    occurred = kwargs.get("occurred")
+    event = kwargs.get("event")
+    payload = kwargs.get("payload")
+    id_val = kwargs.get("id")
+    deployment_parameters = kwargs.get("deployment")
+
+    if isinstance(payload, str):
+        payload = ast.literal_eval(payload)
+    return PrefectEvent(
+        deployment=deployment_parameters,
+        occurred=occurred,
+        event=event,
+        payload=payload,
+        id=id_val,
+    )
 
 
 class PrefectEvent(BaseModel):
     occurred: datetime
     event: str
-    resource: Resource
+    resource: Optional[Any] = None
     related: List[Any] = []
     payload: Dict[str, Any]
     id: UUID
     follows: Optional[UUID] = None
-    received: datetime
+    received: Optional[datetime] = None
+    deployment: Optional[Any] = None
 
-    @field_validator("occurred", "received", mode="before")
-    @classmethod
-    def validate_datetime(cls, v):
-        if isinstance(v, datetime):
-            return v
-        try:
-            return datetime.fromisoformat(v)
-        except (TypeError, ValueError):
-            raise ValueError(f"Invalid datetime format: {v}")
+    @computed_field
+    def message(self) -> dict:
+        return self.payload["message"]
